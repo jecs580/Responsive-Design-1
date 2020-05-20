d5e99ac888d6726c88237599323606f173644bc8
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-05-19 21:01:27 -0400

    Agregando funcionalidad al toggle botton compatible con el modo  dark y light del Sistema Operativo

--- js/app.js
+++ js/app.js
@@ -3,3 +3,14 @@ const checkbox = document.querySelector("#checkbox");
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   checkbox.setAttribute("checked", true);
 }
+checkbox.addEventListener("change", function (event) {
+  if (this.checked) {
+    // console.log("checkeado, modo dark");
+    document.body.classList.remove("is-light-mode");
+    document.body.classList.add("is-dark-mode");
+  } else {
+    // console.log("checkeado, modo light");
+    document.body.classList.remove("is-dark-mode");
+    document.body.classList.add("is-light-mode");
+  }
+});
