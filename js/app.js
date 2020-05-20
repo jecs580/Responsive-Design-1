f012207596007ae615d4af60190834c7d0ee5525
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-05-19 20:24:48 -0400

    Refactor a variables css

--- js/app.js
+++ js/app.js
@@ -2,6 +2,4 @@ const toggle = document.querySelector(".switch");
 const checkbox = document.querySelector("#checkbox");
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   checkbox.setAttribute("checked", true);
-} else {
-  checkbox.setAttribute("checked", false);
 }
